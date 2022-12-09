2150b76e56ab6ba1dd781968dd4a080b8da4243b
Mutsuha Asada <momiimt@gmail.com>
2022-12-09 21:16:58 +0900

    🚧 wip: json backend

--- src/monokaki/builder.nim
+++ src/monokaki/builder.nim
@@ -1,5 +1,9 @@
 import brack
-initBrack()
+import brack/api
+import brack/ast
+
+initExpander(Html)
+initGenerator(Json)
 
 import std/os
 import std/times
@@ -8,6 +12,7 @@ import std/strutils
 import std/sequtils
 import std/algorithm
 import std/sugar
+import std/json
 
 import utils
 import parsetoml
@@ -23,9 +28,11 @@ proc build* () =
     appDir = getAppDir()
   createDir(currentDir / "dist")
   os.copyFile(appDir / "css/style.css", currentDir / "dist/style.css")
+  os.copyFile(appDir / "web/frontend.js", currentDir / "dist/frontend.js")
+  os.copyFile(appDir / "web/index.html", currentDir / "dist/index.html")
   os.copyDir(appDir / "assets/", currentDir / "dist/assets/")
 
-  var pages: seq[Page] = @[]
+  var articles: seq[Page] = @[]
   for (dayInDir, year, month, day) in dateInDir(currentDir / "articles"):
     for dir in walkDir(dayInDir.path):
       let
@@ -40,29 +47,30 @@ proc build* () =
           title,
           overview,
           &"{year}-{month}-{day}",
-          &"{year}/{month}/{day}/{name}.html",
+          &"{year}/{month}/{day}/{name}",
           &"{thumbnail}.png",
           tags
         )
-
-      if not published:
-        continue
 
       block:
         createDir(currentDir / &"dist/{year}/{month}/{day}/")
         for assets in walkFiles(dir.path / "assets"):
           copyFile(assets, currentDir / &"dist/{year}/{month}/{day}/")
-        var outputFile = open(currentDir / &"dist/{year}/{month}/{day}/{name}.html", FileMode.fmWrite)
+        var outputFile = open(currentDir / &"dist/{year}/{month}/{day}/{name}.json", FileMode.fmWrite)
         defer: outputFile.close()
-        let parsed = tokenize(dir.path / "index.[]").parse()
-        outputFile.write(
-          generateArticleHtml(
-            parsed.expand().generate(),
-            page
-          )
-        )
-
-      pages.add page
+        let tokens = tokenize(dir.path / "index.[]")
+        let ast = tokens.parse()
+        var json = ast.expand().generate()
+        json["title"] = newJString(title)
+        json["overview"] = newJString(overview)
+        json["tags"] = newJArray()
+        for tag in tags:
+          json["tags"].add newJString(tag)
+        json["thumbnail"] = newJInt(thumbnail)
+        json["published"] = newJBool(published)
+        outputFile.write(json.pretty)
+      
+      articles.add page
 
   var dailies: seq[Page] = @[]
   for (dir, year, month, day) in dateInDir(currentDir / "dailies"):
@@ -75,45 +83,56 @@ proc build* () =
         &"{year}.{month}.{day}",
         overview,
         &"{year}-{month}-{day}",
-        &"{year}/{month}/{day}/daily.html",
+        &"{year}/{month}/{day}",
         &"{thumbnail}.png",
         @[]
       )
-    
-    if not published:
-      continue
 
     block:
       createDir(currentDir / &"dist/daily/{year}/{month}/{day}/")
       for assets in walkDir(dir.path / "assets/"):
         let name = $assets.path.split('/')[^1]
         copyFile(assets.path, currentDir / &"dist/daily/{year}/{month}/{day}/{name}")
-      var outputFile = open(currentDir / &"dist/daily/{year}/{month}/{day}/daily.html", FileMode.fmWrite)
+      var outputFile = open(currentDir / &"dist/daily/{year}/{month}/{day}/daily.json", FileMode.fmWrite)
       defer: outputFile.close()
-      let parsed = tokenize(dir.path & "/index.[]").parse()
-      outputFile.write(
-        generateDailyHtml(
-          parsed.expand().generate(),
-          page
-        )
-      )
+      var json = tokenize(dir.path / "index.[]").parse().expand().generate()
+      json["overview"] = newJString(overview)
+      json["thumbnail"] = newJInt(thumbnail)
+      json["published"] = newJBool(published)
+      outputFile.write(json.pretty)
     
     dailies.add page
 
   block:
-    var outputFile = open(currentDir / &"dist/index.html", FileMode.fmWrite)
+    var outputFile = open(currentDir / &"dist/articles.json", FileMode.fmWrite)
     defer: outputFile.close()
-    outputFile.write(
-      generateIndexHtml(pages.sorted.reversed)
-    )
+    var json = %* { "articles": [] }
+    for article in articles:
+      json["articles"].add(%* {
+        "title": article.title,
+        "overview": article.overview,
+        "date": article.date,
+        "href": article.href,
+        "thumbnail": article.thumbnail,
+        "tags": article.tags
+      })
+    outputFile.write(json.pretty)
 
   block:
     createDir(currentDir / &"dist/daily/")
-    var outputFile = open(currentDir / &"dist/daily/index.html", FileMode.fmWrite)
+    var outputFile = open(currentDir / &"dist/dailies.json", FileMode.fmWrite)
     defer: outputFile.close()
-    outputFile.write(
-      generateDailyIndexHtml(dailies.sorted.reversed)
-    )
+    var json = %* { "dailies": [] }
+    for daily in dailies:
+      json["dailies"].add(%* {
+        "title": daily.title,
+        "overview": daily.overview,
+        "date": daily.date,
+        "href": daily.href,
+        "thumbnail": daily.thumbnail,
+        "tags": daily.tags
+      })
+    outputFile.write(json.pretty)
 
   let now = now().format("yyyy-MM-dd HH:mm:ss")
   echo &"[{now}] 🎉 Success to build!"
